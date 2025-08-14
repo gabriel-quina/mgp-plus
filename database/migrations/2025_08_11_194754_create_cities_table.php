9e9d1bbf48fb60d01dafe2b05b2194c26bf7456b
Gabriel Quina <gabriel.quina@ymail.com>
2025-08-14 13:29:54 -0300

    feat(db): substitui a tabela alunos com a tabela students e adiciona migrations para states/cities/schools

--- database/migrations/2025_08_11_194754_create_cities_table.php
+++ database/migrations/2025_08_11_194754_create_cities_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('alunos', function (Blueprint $table) {
+        Schema::create('cities', function (Blueprint $table) {
             $table->id();
-            $table->string('nome');
-            $table->string('cpf')->unique();
-            $table->string('email')->unique();
-            $table->string('matricula')->unique();
-            $table->string('ano_escolar');
+            $table->foreignId('state_id')->constrained('states')->cascadeOnUpdate()->restrictOnDelete();
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('alunos');
+        Schema::dropIfExists('cities');
     }
 };
