6ce94322d7496c6860885e38742e42bc992d3599
Gabriel Quina <68925625+gabriel-quina@users.noreply.github.com>
2026-01-14 17:53:07 -0300

    Fix students show route conflict

--- routes/web/school/students.php
+++ routes/web/school/students.php
@@ -3,10 +3,11 @@
 use App\Http\Controllers\Schools\SchoolStudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('alunos/{student}', [SchoolStudentController::class, 'show'])
-    ->name('students.show');
-
 Route::resource('alunos', SchoolStudentController::class)
     ->only(['index', 'create', 'store', 'edit', 'update'])
     ->names('students')
     ->parameters(['alunos' => 'student']);
+
+Route::get('alunos/{student}', [SchoolStudentController::class, 'show'])
+    ->whereNumber('student')
+    ->name('students.show');
