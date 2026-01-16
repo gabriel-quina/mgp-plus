9ee201ddd05429b9ef6015368f5237ebcb22a309
Gabriel Quina <68925625+gabriel-quina@users.noreply.github.com>
2026-01-16 12:23:50 -0300

    Add SchoolController wrapper for admin routes

---
+++ app/Http/Controllers/SchoolController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Schools\SchoolController as TenantSchoolController;
+
+class SchoolController extends TenantSchoolController
+{
+}
