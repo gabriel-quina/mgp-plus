21bf3a390e42f9a8d0c684a3035c6fced5f0070b
Gabriel Quina <68925625+gabriel-quina@users.noreply.github.com>
2026-01-16 10:46:04 -0300

    Apply school access middleware to tenant routes

--- routes/web/school/dashboard.php
+++ routes/web/school/dashboard.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\Schools{
-    SchoolController
-};
+use App\Http\Controllers\Schools\SchoolDashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('escolas', SchoolController::class)
-    ->names('schools')
-    ->parameters(['escolas' => 'school']);
+Route::get('/', [SchoolDashboardController::class, 'show'])
+    ->name('dashboard');
